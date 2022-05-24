e4b0ff1106f59ec9e80fe2680d1e2c4a9b108ad4
wcchin <wcchin.88@gmail.com>
2022-05-24 14:45:57 +0800

    update publications

--- resources/js/figure_data.js
+++ resources/js/figure_data.js
@@ -1,7 +1,7 @@
 var publicationByYear = {
   year:        [2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022], 
   article:     [   0,    1,    2,    1,    2,    0,    2,    3,    0,    1], 
-  bookchapter: [   0,    0,    0,    1,    0,    0,    0,    0,    2,    0], 
+  bookchapter: [   0,    0,    0,    1,    0,    0,    0,    0,    2,    2], 
   other:       [   1,    0,    0,    1,    0,    1,    0,    1,    1,    0], 
   color: {
     article: "#82cafc", 
